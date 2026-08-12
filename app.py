abdd6e00be8cd38fbd4f3c2d3b4e29bfc4e8dac7
Sameer Shaikh 2 <demo18887@gmail.com>
2026-08-12 10:05:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
+with open("model.pkl", "rb") as file:
+    model = pickle.load(file)
+    
 @app.route('/')
 def home():
     return render_template('index.html')
